e5efd8e1d18990e8ddaed0c468fa12097c78b5d2
HADDAD Moez <mhaddad@ats-digital.com>
2021-01-22 12:56:01 +0100

    refactor: post link

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -3,7 +3,7 @@ import { Link } from '../models/link';
 const post = async (_, { description, url }) => {
   const link = new Link({ description, url });
 
-  link.save((err) => {
+  await link.save((err) => {
     if (err) return err;
   });
 
