92ad6dd03aabf2777b88991c0f5cff27726f255b
HADDAD Moez <mhaddad@ats-digital.com>
2021-01-26 13:50:43 +0100

    feat: url validation

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,13 +1,13 @@
 import { Link } from '../models/link';
+import { ApolloError } from 'apollo-server';
+import { StatusCodes } from 'http-status-codes';
 
 const post = async (_, { description, url }) => {
   const link = new Link({ description, url });
 
-  await link.save((err) => {
-    if (err) return err;
-  });
-
-  return link;
+  return await link
+    .save()
+    .catch((err) => new ApolloError(err, StatusCodes.BAD_REQUEST));
 };
 
 export { post };
