95cd03fd270ca85e327d0bd10b2eb48450260f31
HADDAD Moez <mhaddad@ats-digital.com>
2021-02-17 19:55:59 +0100

    refactor: update index

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server';
 import db from './utils/db';
 import { PORT, HOST } from './constants';
 import { typeDefs } from './schemas';
 import { resolvers } from './resolvers';
-
-const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-server.applyMiddleware({ app });
-
 db.connect();
 
-app.use((req, res) => {
-  res.status(200);
-  res.send('Hello World!');
-  res.end();
-});
-
-app.listen({ port: PORT }, () =>
-  console.log(`🚀 Server ready at http://${HOST}:${PORT}${server.graphqlPath}`)
-);
+server.listen().then(({ url }) => console.log(`🚀 Server is running on ${url}`));
