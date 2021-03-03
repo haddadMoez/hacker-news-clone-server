9242c495430d11b27c372f0a954f3c37427d2779
HADDAD Moez <mhaddad@ats-digital.com>
2021-03-03 14:09:41 +0100

    feat: allow internal errors graphql shield

--- src/utils/auth/permissions.js
+++ src/utils/auth/permissions.js
@@ -1,9 +1,14 @@
 import { shield } from 'graphql-shield';
 import isAuthenticated from './rules';
 
-export default shield({
-  Mutation: {
-    vote: isAuthenticated,
-    post: isAuthenticated,
+export default shield(
+  {
+    Mutation: {
+      vote: isAuthenticated,
+      post: isAuthenticated,
+    },
   },
-});
+  {
+    allowExternalErrors: true,
+  }
+);
